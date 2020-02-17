46f3635e97121f563e7402085a5a3e592968900c
kazuma <kazumazuma1124@gmail.com>
2020-02-17 22:11:50 +0900

    rewardのパーセント追加

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Icon(Icons.cloud)));
+    return Scaffold(body: Center(child: 
+    CircularPercentIndicator(
+                  radius: 60.0,
+                  lineWidth: 5.0,
+                  percent: 0.3,
+                  center: Text("30%"),
+                  progressColor: Colors.blue,
+                ))
+    
+    );
   }
 }
