ca42f99e25cfa8902870a85e5ea5a16a53dfc5e8
kazuma <kazumazuma1124@gmail.com>
2020-02-19 22:06:53 +0900

    設定ページの作成

--- lib/Model/ColorModel.dart
+++ lib/Model/ColorModel.dart
@@ -1,54 +1,54 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class ColorModel extends ChangeNotifier {
-//   Color themaColor = Colors.blue;
-//   void setBlue() {
-//     themaColor = Colors.blue;
-//     notifyListeners();
-//   }
+class ColorModel extends ChangeNotifier {
+  Color themaColor = Colors.blue;
+  void setBlue() {
+    themaColor = Colors.blue;
+    notifyListeners();
+  }
 
-//   void setRed() {
-//     themaColor = Colors.red;
-//     notifyListeners();
-//   }
+  void setRed() {
+    themaColor = Colors.red;
+    notifyListeners();
+  }
 
-//   void setPurple() {
-//     themaColor = Colors.purple;
-//     notifyListeners();
-//   }
+  void setPurple() {
+    themaColor = Colors.purple;
+    notifyListeners();
+  }
 
-//   void setPink() {
-//     themaColor = Colors.pink;
-//     notifyListeners();
-//   }
+  void setPink() {
+    themaColor = Colors.pink;
+    notifyListeners();
+  }
 
-//   void setOrange() {
-//     themaColor = Colors.orange;
-//     notifyListeners();
-//   }
+  void setOrange() {
+    themaColor = Colors.orange;
+    notifyListeners();
+  }
 
-//   void setCyan() {
-//     themaColor = Colors.cyan;
-//     notifyListeners();
-//   }
+  void setCyan() {
+    themaColor = Colors.cyan;
+    notifyListeners();
+  }
 
-//   void setTeal() {
-//     themaColor = Colors.teal;
-//     notifyListeners();
-//   }
+  void setTeal() {
+    themaColor = Colors.teal;
+    notifyListeners();
+  }
 
-//   void setIndigo() {
-//     themaColor = Colors.indigo;
-//     notifyListeners();
-//   }
+  void setIndigo() {
+    themaColor = Colors.indigo;
+    notifyListeners();
+  }
 
-//   void setBrown() {
-//     themaColor = Colors.brown;
-//     notifyListeners();
-//   }
+  void setBrown() {
+    themaColor = Colors.brown;
+    notifyListeners();
+  }
 
-//   void setYellow() {
-//     themaColor = Colors.yellow;
-//     notifyListeners();
-//   }
-// }
+  void setYellow() {
+    themaColor = Colors.yellow;
+    notifyListeners();
+  }
+}
--- lib/view/SettingPage.dart
+++ lib/view/SettingPage.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Icon(Icons.cloud)));
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+        body: Column(
+      mainAxisSize: MainAxisSize.max,
+      // mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Flexible(
+            child: ListView(children: [
+          Text(""),
+          Text("　設定"),
+          _menuItem("パスワード設定", Icon(Ionicons.ios_lock)),
+          _menuItem("メニュー2", Icon(Icons.map)),
+          _menuItem("メニュー3", Icon(Icons.room)),
+          _menuItem("メニュー4", Icon(Icons.local_shipping)),
+          Text(""),
+          Text("　アプリについて"),
+          _menuItem("メニュー2", Icon(Icons.map)),
+          _menuItem("メニュー3", Icon(Icons.room)),
+          _menuItem("メニュー4", Icon(Icons.local_shipping)),
+          _menuItem("メニュー5", Icon(Icons.airplanemode_active)),
+        ])),
+      ],
+    ));
+  }
+
+  Widget _menuItem(String title, Icon icon) {
+    return GestureDetector(
+      child: Container(
+          padding: EdgeInsets.all(8.0),
+          decoration: new BoxDecoration(
+            color: Colors.white,
+              border: new Border(
+                  bottom: BorderSide(width: 1.0, color: Colors.grey))),
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(5.0),
+                child: icon,                
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          )),
+      onTap: () {
+        print("onTap called.");
+      },
+    );
   }
 }
