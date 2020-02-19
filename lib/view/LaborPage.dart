b669ae77c121d3ad17b052ec2b81a47ca9048cc0
kazuma <kazumazuma1124@gmail.com>
2020-02-19 16:08:56 +0900

    お手伝い追加画面作成

--- lib/view/LaborPage.dart
+++ lib/view/LaborPage.dart
@@ -1,27 +1,17 @@
 import 'package:dama_app/model/LaborModel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
 
 class LaborPage extends StatelessWidget {
+  Labor tmpLabor = null;
+
   @override
   Widget build(BuildContext context) {
     return Consumer<LaborModel>(builder: (context, rewardModel, child) {
       return Scaffold(
-          body: 
-        //   Column(
-        // children: <Widget>[
-        //   TextField(
-        //     enabled: true,
-        //     // 入力数
-        //     maxLength: 10,
-        //     maxLengthEnforced: false,
-        //     style: TextStyle(color: Colors.red),
-        //     obscureText: false,
-        //     maxLines: 1,
-        //     //パスワード
-        //     // onChanged: _handleText,
-        //   ),
-          ListView.builder(
+          body: ListView.builder(
             itemBuilder: (BuildContext context, int index) {
               return Card(
                 child: Padding(
@@ -36,9 +26,58 @@ class LaborPage extends StatelessWidget {
             },
             itemCount: rewardModel.laborList.length,
           ),
-        // ],
-      // )
-      );
+          floatingActionButton: FloatingActionButton(
+              child: Icon(Ionicons.ios_add),
+              onPressed: () {
+                showDialog<String>(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (BuildContext context) {
+                      // String result = '';
+                      return AlertDialog(
+                        title: Text('新規追加しますか？'),
+                        content: Column(children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: TextField(
+                                autofocus: true,
+                                decoration: InputDecoration(
+                                  labelText: "名前",
+                                  // hintText: "test"
+                                ),
+                                onChanged: (value) {
+                                  tmpLabor.name = value;
+                                },
+                              )),
+                            ],
+                          ),
+                          Expanded(
+                              child: TextField(
+                                  decoration: InputDecoration(
+                                      labelText: "ポイント"),
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                WhitelistingTextInputFormatter.digitsOnly
+                              ]))
+                        ]),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Ok'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    });
+              }));
     });
   }
 }
