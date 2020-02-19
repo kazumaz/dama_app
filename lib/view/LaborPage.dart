f9c1d0f386499b068f240825052ecc43fdee0d13
kazuma <kazumazuma1124@gmail.com>
2020-02-19 15:27:33 +0900

    微修正

--- lib/view/LaborPage.dart
+++ lib/view/LaborPage.dart
@@ -1,15 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class  LaborPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(body: Center(child: Icon(Icons.cloud)));
-//   }
-// }
-
-
-
-
 import 'package:dama_app/model/LaborModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,21 +5,39 @@ import 'package:provider/provider.dart';
 class LaborPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<LaborModel>(builder: (context, rewardModel, child) {      
+    return Consumer<LaborModel>(builder: (context, rewardModel, child) {
       return Scaffold(
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Padding(
-                child: Text(
-                  rewardModel.laborList[index].point.toString() + " Point:　　" + rewardModel.laborList[index].name,                  
+          body: 
+        //   Column(
+        // children: <Widget>[
+        //   TextField(
+        //     enabled: true,
+        //     // 入力数
+        //     maxLength: 10,
+        //     maxLengthEnforced: false,
+        //     style: TextStyle(color: Colors.red),
+        //     obscureText: false,
+        //     maxLines: 1,
+        //     //パスワード
+        //     // onChanged: _handleText,
+        //   ),
+          ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Padding(
+                  child: Text(
+                    rewardModel.laborList[index].point.toString() +
+                        " Point:　　" +
+                        rewardModel.laborList[index].name,
+                  ),
+                  padding: EdgeInsets.all(20.0),
                 ),
-                padding: EdgeInsets.all(20.0),
-              ),
-            );
-          },
-          itemCount: rewardModel.laborList.length,
-        ),
+              );
+            },
+            itemCount: rewardModel.laborList.length,
+          ),
+        // ],
+      // )
       );
     });
   }
