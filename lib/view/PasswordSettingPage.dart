6a16fdce9d504abd738f54ccdbfefc733f58fded
kazuma <kazumazuma1124@gmail.com>
2020-02-20 00:50:52 +0900

    パスワード入力画面修正

--- lib/view/PasswordSettingPage.dart
+++ lib/view/PasswordSettingPage.dart
@@ -5,39 +5,49 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class PassWordSettingPage extends StatelessWidget {
-
   final passwordInputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Consumer<PasswordModel>(builder: (context, passwordModel, child) {
       return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () => Navigator.of(context).pushReplacementNamed("/home"),
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () =>
+                  Navigator.of(context).pushReplacementNamed("/home"),
+            ),
+            backgroundColor: Theme.of(context).primaryColor,
+            elevation: 0.0,
+            title: Text("パスワード変更画面"), //TODO タイトルを正しくする
           ),
-          backgroundColor: Theme.of(context).primaryColor,
-          elevation: 0.0,
-          title: Text("パスワード変更画面"), //TODO タイトルを正しくする
-        ),
-        body: Center(child: _passwordForm()),
-      );
+          body: Column(
+            children: <Widget>[_passwordForm(), _passwordConfirmForm()],
+          ));
     });
   }
 
-Widget _passwordForm() {
+  Widget _passwordForm() {
     return TextFormField(
       controller: passwordInputController,
       autocorrect: false,
       decoration: new InputDecoration(
         border: const UnderlineInputBorder(),
-        labelText: 'Password',
+        labelText: 'パスワード',
       ),
       obscureText: true,
     );
   }
 
+  Widget _passwordConfirmForm() {
+    return TextFormField(
+      controller: passwordInputController,
+      autocorrect: false,
+      decoration: new InputDecoration(
+        border: const UnderlineInputBorder(),
+        labelText: '確認用',
+      ),
+      obscureText: true,
+    );
+  }
 }
-
-
