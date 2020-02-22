665bf36bb5add2d79bad6685e4ad15c2773f5a2e
kazuma <kazumazuma1124@gmail.com>
2020-02-22 18:52:10 +0900

    ご褒美追加ボタンを追加

--- lib/Model/RewardModel.dart
+++ lib/Model/RewardModel.dart
@@ -3,21 +3,22 @@ import 'package:flutter/material.dart';
 class RewordModel extends ChangeNotifier {
 
   List<Reward> rewardList = [ 
-    Reward(key: 1, name: "お菓子", point: 3),
-    Reward(key: 2, name: "遊園地", point: 40),
-    Reward(key: 3, name: "ディズニー", point: 10000),
-    Reward(key: 4, name: "おもちゃ", point: 10),
-    Reward(key: 4, name: "うまい棒", point: 1),
+    Reward(name: "お菓子", point: 3),
+    Reward(name: "遊園地", point: 40),
+    Reward(name: "ディズニー", point: 10000),
+    Reward(name: "おもちゃ", point: 10),
+    Reward(name: "うまい棒", point: 1),
   ];
 
-
-  // List<Reward> rewardList = []; //Todo 永続化先から読み込めるようにする。
-  int nextRewardKey = 0; //Todo 永続化先から読み込めるようにする。
+  // List<Reward> rewardList = []; //Todo 永続化先から読み込めるようにする。  
 
   void addReward({@required String name, @required int point}) {
-    rewardList.add(Reward(key: nextRewardKey, name: name, point: point));
-    nextRewardKey++;
-    print(nextRewardKey); //ToDo　デバッグコードなので削除
+    rewardList.add(Reward(name: name, point: point));    
+    notifyListeners();
+  }
+
+  void addRewardModel({@required Reward reward}) {
+    rewardList.add(reward);    
     notifyListeners();
   }
 
@@ -27,19 +28,16 @@ class RewordModel extends ChangeNotifier {
 }
 
 class Reward {
-  final int key;
   String name;
   int point;
 
-  Reward({@required this.key, @required this.name, @required this.point});
+  Reward({@required this.name, @required this.point});
 
   Reward.fromJson(Map<String, dynamic> json)
-      : key = json['key'],
-        name = json['name'],
+      : name = json['name'],
         point = json['point'];
 
-  Map<String, dynamic> toJson() => {
-        'key': key,
+  Map<String, dynamic> toJson() => {        
         'name': name,
         'point': point,
       };
--- lib/view/RewardPage.dart
+++ lib/view/RewardPage.dart
@@ -1,8 +1,13 @@
 import 'package:dama_app/model/RewardModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class RewardPage extends StatelessWidget {
+  final myRewardNameController = TextEditingController();
+  final myRewardPointController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<RewordModel>(builder: (context, rewardModel, child) {      
@@ -20,6 +25,84 @@ class RewardPage extends StatelessWidget {
           },
           itemCount: rewardModel.rewardList.length,
         ),
+          floatingActionButton: FloatingActionButton(
+              child: Icon(Ionicons.ios_add),
+              onPressed: () {
+                showDialog<String>(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (BuildContext context) {
+                      // String result = '';
+                      return AlertDialog(
+                        title: Text('新規追加しますか？'),
+                        content: Column(children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: TextField(
+                                controller: myRewardNameController,
+                                autofocus: true,
+                                
+                                decoration: InputDecoration(
+                                  labelText: "名前",
+                                  // hintText: "test"
+                                ),
+                                maxLength: 10,
+                                onChanged: (value) {
+                                  // nothing to do
+                                },
+                              )),
+                            ],
+                          ),
+                          Expanded(
+                              child: TextField(
+                                  controller: myRewardPointController,
+                                  decoration:
+                                      InputDecoration(labelText: "ポイント"),
+                                      maxLength: 5,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                WhitelistingTextInputFormatter.digitsOnly
+                              ]))
+                        ]),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              myRewardNameController.clear();
+                              myRewardPointController.clear();
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Ok'),
+                            onPressed: () {
+                              Reward tmpLabor = Reward(
+                                  name: myRewardNameController.text,
+                                  point:
+                                      int.parse(myRewardPointController.text));
+
+                              if (myRewardNameController.text == null ||
+                                  myRewardPointController.text == null ||
+                                  myRewardNameController.text == "" ||
+                                  myRewardPointController.text == "null") {
+                                myRewardNameController.clear();
+                                myRewardPointController.clear();
+                                Navigator.of(context).pop();
+                              }
+                              print(myRewardNameController.text);
+                              print(myRewardPointController.text);
+
+                              rewardModel.addRewardModel(reward: tmpLabor);
+                              myRewardNameController.clear();
+                              myRewardPointController.clear();
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    });
+              })
       );
     });
   }
