f7efd48cc827d040a828d475300a8577c1b10db7
kazuma <kazumazuma1124@gmail.com>
2020-02-28 22:03:00 +0900

    ロック機能対象画面はパスワード設定がある時のみに変更

--- lib/view/SettingPage.dart
+++ lib/view/SettingPage.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:dama_app/model/PasswordModel.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:share/share.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
+  final passwordInputController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("設定"), actions: <Widget>[
-          // IconButton(icon: Icon(Icons.help_outline), onPressed: () {})
-        ]),
-        backgroundColor: Colors.grey[100],
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Flexible(
-                child: ListView(children: [
-              Text(""),
-              Text("　設定"),
-              _passwordSetting("　パスワード設定", Icon(Ionicons.ios_lock), context),
-              _colorSetting("　テーマカラーの変更", Icon(Icons.map), context),
-              _pointChange("　ポイント強制修正", Icon(Icons.room), context),
-              _rockSetting("　ロック対象機能設定", Icon(Icons.local_shipping), context),
-              Text(""),
-              Text("　アプリについて"),
-              _opinionPage("　ご意見・ご要望など", Icon(Entypo.pencil), context),
-              _twitterPage("　開発者のツート", Icon(FontAwesome.twitter), context),
-              _appShare("　このアプリを紹介", Icon(Ionicons.ios_rocket), context),
-              _appReview("　このアプリを評価", Icon(Icons.star), context),
-              _donate("　寄付", Icon(MaterialCommunityIcons.gift), context),
-            ])),
-          ],
-        ));
+    return Consumer<PasswordModel>(builder: (context, passwordModel, child) {
+      return Scaffold(
+          appBar: AppBar(title: Text("設定"), actions: <Widget>[
+            // IconButton(icon: Icon(Icons.help_outline), onPressed: () {})
+          ]),
+          backgroundColor: Colors.grey[100],
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Flexible(
+                  child: ListView(children: [
+                Text(""),
+                Text("　設定"),
+                _passwordSetting("　パスワード設定", Icon(Ionicons.ios_lock), context),
+                _colorSetting("　テーマカラーの変更", Icon(Icons.map), context),
+                _pointChange("　ポイント強制修正", Icon(Icons.room), context),
+                _rockSetting("　ロック対象機能設定", Icon(Icons.local_shipping), context,
+                    passwordModel),
+                Text(""),
+                Text("　アプリについて"),
+                _opinionPage("　ご意見・ご要望など", Icon(Entypo.pencil), context),
+                _twitterPage("　開発者のツート", Icon(FontAwesome.twitter), context),
+                _appShare("　このアプリを紹介", Icon(Ionicons.ios_rocket), context),
+                _appReview("　このアプリを評価", Icon(Icons.star), context),
+                _donate("　寄付", Icon(MaterialCommunityIcons.gift), context),
+              ])),
+            ],
+          ));
+    });
   }
 
-  Widget _rockSetting(String title, Icon icon, BuildContext context) {
+  Widget _rockSetting(String title, Icon icon, BuildContext context,
+      PasswordModel passwordModel) {
     return GestureDetector(
       child: Container(
           padding: EdgeInsets.all(8.0),
@@ -59,9 +67,74 @@ class SettingsPage extends StatelessWidget {
             ],
           )),
       onTap: () {
-        // Navigator.of(context).pushReplacementNamed("/rockSetting");
-        Navigator.of(context).pushNamed("/rockSetting");
-        print("onTap called.");
+        if (passwordModel.checkPassworIsAlreadyExist()) {
+          AwesomeDialog(
+            context: context,
+            animType: AnimType.SCALE,
+            dialogType: DialogType.INFO,
+            aligment: Alignment.topCenter,
+            body: Center(
+              child: Column(children: <Widget>[
+                Container(
+                    child: Text(
+                  'パスワードを入力してください。',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                )),
+                Container(
+                    width: 200.0,
+                    child: TextField(
+                        controller: passwordInputController, autofocus: true
+                        // decoration: InputDecoration(
+                        //   // labelText: "編集する場合は入力してください",
+
+                        ))
+              ]),
+            ),
+            tittle: 'This is Ignored',
+            desc: 'This is also Ignored',
+            btnOkOnPress: () {
+              if (passwordModel.isValidPassword(
+                  checkPassword: passwordInputController.text.toString())) {
+                Navigator.of(context).pushNamed("/rockSetting");
+              } else {
+                AwesomeDialog(
+                  context: context,
+                  animType: AnimType.SCALE,
+                  dialogType: DialogType.ERROR,
+                  body: Center(
+                    child: Text(
+                      'パスワードエラーです。入力し直してください。',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  tittle: 'This is Ignored',
+                  desc: 'This is also Ignored',
+                  btnOkOnPress: () {},
+                ).show();
+              }
+            },
+          ).show();
+        } else {
+          AwesomeDialog(
+            context: context,
+            animType: AnimType.SCALE,
+            dialogType: DialogType.ERROR,
+            body: Center(
+              child: Text(
+                'ロック機能を使う際は、先に「パスワード設定画面」にて、パスワードの設定を行ってください。',
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+            ),
+            tittle: 'This is Ignored',
+            desc: 'This is also Ignored',
+            btnCancelText: "やめとく",
+            btnOkText: "設定する",
+            btnCancelOnPress: () {},
+            btnOkOnPress: () {
+              Navigator.of(context).pushNamed("/passwordSetting");
+            },
+          ).show();
+        }
       },
     );
   }
