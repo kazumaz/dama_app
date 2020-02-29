ecc6d9a9ce45370a56bfc8bf836b3537e0cafa06
kazuma <kazumazuma1124@gmail.com>
2020-02-29 14:17:15 +0900

    色選択が切れるバグを修正

--- lib/parts/ColorPicker.dart
+++ lib/parts/ColorPicker.dart
@@ -7,111 +7,111 @@ class SelectionScreen extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
         body: GridView.count(
             crossAxisCount: 5, // 1行に表示する数
-            crossAxisSpacing: 10.0, // 縦スペース
-            mainAxisSpacing: 10.0, // 横スペース
+            crossAxisSpacing: 8.0, // 縦スペース
+            mainAxisSpacing: 8.0, // 横スペース
             shrinkWrap: true,
             children: <Widget>[
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.blue),
-                iconSize: 70.0,
+                iconSize: 60.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setBlue();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.red),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setRed();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.purple),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setPurple();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.pink),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setPink();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.orange),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setOrange();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.cyan),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setCyan();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.teal),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setTeal();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.indigo),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setIndigo();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.brown),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setBrown();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.yellow),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setYellow();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.lightGreen),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setLightGreen();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.grey),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setGrey();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.deepPurple),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setDeepPurple();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.deepOrange),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setDeepOrange();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.lens, color: Colors.blueGrey),
-                iconSize: 70.0,
+                iconSize: 65.0,
                 onPressed: () {
                   Provider.of<ColorModel>(context, listen: false).setBlueGray();
                 },
